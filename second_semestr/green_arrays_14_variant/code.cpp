86b07bd8f3b9d5b0172fe6c7ef9af79f2500e71d
Konovalov Viktor <Viktor.Konovalov.130807@gmail.com>
2026-02-08 00:34:50 +0500

    Add files via upload

---
+++ second_semestr/green_arrays_14_variant/code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void show(int arr[100], int N) { // вывод массива
+	for (int i = 0; i < N; i++) { 
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	srand(time(0));
+
+	int N = 23; 
+	int arr[100];
+
+
+	for (int i = 0; i < N; i++) { // инициализация
+		arr[i] = ((float)rand() / RAND_MAX)*100;
+	}
+	show(arr, N);
+
+
+	if (N % 2 != 0) {
+		for (int i = N/2; i < N; i++) {
+			arr[i] = arr[i + 1]; 
+		}
+		N--;
+	}
+	show(arr, N);
+
+
+	for (int i = N; i >= 0; i--) { // сдвиг на 3 элемента чтобы влезли 3 элемента вначале
+		arr[i + 3] = arr[i];
+	}
+	N += 3;
+	for (int I = 0; I < 3; I++) {
+		arr[I] = arr[I + 10+3] - 2;
+	}
+	show(arr, N);
+
+
+}
